e506e83221be3967291c554222172b528e187823
yuxuanhuang <yuxuanhuang@autox.ai>
2025-03-12 20:39:03 +0800

    fix resnet dcn bug

--- configs/bevformer/bevformer_base.py
+++ configs/bevformer/bevformer_base.py
@@ -7,7 +7,7 @@ backbone=dict(
     norm_cfg=dict(type='BN2d', requires_grad=False),
     norm_eval=True,
     style='caffe',
-    # dcn=dict(type='DCNv2', deform_groups=1, fallback_on_stride=False), # original DCNv2 will print log when perform load_state_dict
+    dcn=dict(type='DCNv2', deform_groups=1, fallback_on_stride=False), # original DCNv2 will print log when perform load_state_dict
     stage_with_dcn=(False, False, True, True))
 
 
--- mmhdmap/models/backbones/resnet.py
+++ mmhdmap/models/backbones/resnet.py
@@ -4,6 +4,7 @@ import warnings
 import torch.nn as nn
 import torch.utils.checkpoint as cp
 from mmcv.cnn import build_conv_layer, build_norm_layer, build_plugin_layer
+from mmcv.ops import ModulatedDeformConv2dPack
 from mmengine.model import BaseModule
 from torch.nn.modules.batchnorm import _BatchNorm
 
