35f9307b7fc31b982a7e98c7426d2ebdc03063a0
yuxuanhuang <yuxuanhuang@autox.ai>
2025-11-25 17:50:20 +0400

    add detr train code

--- projects/detection/models/dense_heads/detr_head.py
+++ projects/detection/models/dense_heads/detr_head.py
@@ -113,6 +113,198 @@ class DETRHead(BaseModule):
             self.activate(self.reg_ffn(hidden_states))).sigmoid()
         return layers_cls_scores, layers_bbox_preds
     
+    def loss(self, hidden_states: Tensor,
+             batch_data_samples: SampleList) -> dict:
+        
+        batch_gt_instances = []
+        batch_img_metas = []
+        for data_sample in batch_data_samples:
+            batch_img_metas.append(data_sample.metainfo)
+            batch_gt_instances.append(data_sample.gt_instances)
+    
+        outs = self(hidden_states)
+        loss_inputs = outs + (batch_gt_instances, batch_img_metas)
+        losses = self.loss_by_feat(*loss_inputs)
+        return losses
+        
+    def loss_by_feat(
+        self,
+        all_layers_cls_scores: Tensor,
+        all_layers_bbox_preds: Tensor,
+        batch_gt_instances: InstanceList,
+        batch_img_metas: List[dict],
+        batch_gt_instances_ignore: OptInstanceList = None
+    ) -> Dict[str, Tensor]:
+        
+        assert batch_gt_instances_ignore is None, \
+            f'{self.__class__.__name__} only supports ' \
+            'for batch_gt_instances_ignore setting to None.'
+        
+        losses_cls, losses_bbox, losses_iou = multi_apply(
+            self.loss_by_feat_single,
+            all_layers_cls_scores,
+            all_layers_bbox_preds,
+            batch_gt_instances=batch_gt_instances,
+            batch_img_metas=batch_img_metas)
+        
+        loss_dict = dict()
+        loss_dict['loss_cls'] = losses_cls[-1]
+        loss_dict['loss_bbox'] = losses_bbox[-1]
+        loss_dict['loss_iou'] = losses_iou[-1]
+
+        num_dec_layer = 0
+        for loss_cls_i, loss_bbox_i, loss_iou_i in \
+            zip(losses_cls[:-1], losses_bbox[:-1], losses_iou[:-1]):
+            loss_dict[f'd{num_dec_layer}.loss_cls'] = loss_cls_i
+            loss_dict[f'd{num_dec_layer}.loss_bbox'] = loss_bbox_i
+            loss_dict[f'd{num_dec_layer}.loss_iou'] = loss_iou_i
+            num_dec_layer += 1
+        
+        return loss_dict
+
+    def loss_by_feat_single(self, cls_scores: Tensor, bbox_preds: Tensor,
+                            batch_gt_instances: InstanceList,
+                            batch_img_metas: List[dict]) -> Tuple[Tensor]:
+        num_imgs = cls_scores.size(0)
+        cls_scores_list = [cls_scores[i] for i in range(num_imgs)]
+        bbox_preds_list = [bbox_preds[i] for i in range(num_imgs)]
+        cls_reg_targets = self.get_targets(cls_scores_list, bbox_preds_list,
+                                           batch_gt_instances, batch_img_metas)
+        (labels_list, label_weights_list, bbox_targets_list, bbox_weights_list,
+         num_total_pos, num_total_neg) = cls_reg_targets
+        labels = torch.cat(labels_list, 0)
+        label_weights = torch.cat(label_weights_list, 0)
+        bbox_targets = torch.cat(bbox_targets_list, 0)
+        bbox_weights = torch.cat(bbox_weights_list, 0)
+
+        cls_scores = cls_scores.reshape(-1, self.cls_out_channels)
+        cls_avg_factor = num_total_pos * 1.0 + \
+            num_total_neg * self.bg_cls_weight
+        if self.sync_cls_avg_factor:
+            cls_avg_factor = reduce_mean(
+                cls_scores.new_tensor([cls_avg_factor]))
+        cls_avg_factor = max(cls_avg_factor, 1)
+
+        if isinstance(self.loss_cls, QualityFocalLoss):
+            bg_class_ind = self.num_classes
+            pos_inds = ((labels >= 0)
+                        & (labels < bg_class_ind)).nonzero().squeeze(1)
+            scores = label_weights.new_zeros(labels.shape)
+            pos_bbox_targets = bbox_targets[pos_inds]
+            pos_decode_bbox_targets = bbox_cxcywh_to_xyxy(pos_bbox_targets)
+            pos_bbox_pred = bbox_preds.reshape(-1, 4)[pos_inds]
+            pos_decode_bbox_pred = bbox_cxcywh_to_xyxy(pos_bbox_pred)
+            scores[pos_inds] = bbox_overlaps(
+                pos_decode_bbox_pred.detach(),
+                pos_decode_bbox_targets,
+                is_aligned=True)
+            loss_cls = self.loss_cls(
+                cls_scores, (labels, scores),
+                label_weights,
+                avg_factor=cls_avg_factor)
+        else:
+            loss_cls = self.loss_cls(
+                cls_scores, labels, label_weights, avg_factor=cls_avg_factor)
+        
+        num_total_pos = loss_cls.new_tensor([num_total_pos])
+        num_total_pos = torch.clamp(reduce_mean(num_total_pos), min=1).item()
+
+        factors = []
+        for img_meta, bbox_pred in zip(batch_img_metas, bbox_preds):
+            img_h, img_w, = img_meta['img_shape']
+            factor = bbox_pred.new_tensor([img_w, img_h, img_w,
+                                           img_h]).unsqueeze(0).repeat(
+                                            bbox_pred.size(0), 1)
+            factors.append(factor)
+        factors = torch.cat(factors, 0)
+
+        bbox_preds = bbox_preds.reshape(-1, 4)
+        bboxes = bbox_cxcywh_to_xyxy(bbox_preds) * factors
+        bboxes_gt = bbox_cxcywh_to_xyxy(bbox_targets) * factors
+
+        loss_iou = self.loss_iou(
+            bboxes, bboxes_gt, bbox_weights, avg_factor=num_total_pos)
+        
+        loss_bbox = self.loss_bbox(
+            bbox_preds, bbox_targets, bbox_weights, avg_factor=num_total_pos)
+        
+        return loss_cls, loss_bbox, loss_iou
+
+    def get_targets(self, cls_scores_list: List[Tensor],
+                    bbox_preds_list: List[Tensor],
+                    batch_gt_instances: InstanceList,
+                    batch_img_metas: List[dict]) -> tuple:
+        (labels_list, label_weights_list, bbox_targets_list, bbox_weights_list,
+         pos_inds_list, neg_inds_list) = multi_apply(self._get_targets_single,
+                                                     cls_scores_list, bbox_preds_list,
+                                                     batch_gt_instances, batch_img_metas)
+        num_total_pos = sum((inds.numel() for inds in pos_inds_list))
+        num_total_neg = sum((inds.numel() for inds in neg_inds_list))
+
+        return (labels_list, label_weights_list, bbox_targets_list,
+                bbox_weights_list, num_total_pos, num_total_neg)
+
+    def _get_targets_single(self, cls_score: Tensor, bbox_pred: Tensor,
+                           gt_instances: InstanceData, img_meta: dict) -> tuple:
+        img_h, img_w = img_meta['img_shape']
+        factor = bbox_pred.new_tensor([img_w, img_h, img_w, img_h]).unsqueeze(0)
+        num_bboxes = bbox_pred.size(0)
+
+        bbox_pred = bbox_cxcywh_to_xyxy(bbox_pred)
+        bbox_pred = bbox_pred * factor
+
+        pred_instances = InstanceData(scores=cls_score, bboxes=bbox_pred)
+
+        assign_result = self.assigner.assign(
+            pred_instances=pred_instances,
+            gt_instances=gt_instances,
+            img_meta=img_meta)
+        
+        gt_bboxes = gt_instances.bboxes
+        gt_labels = gt_instances.labels
+        pos_inds = torch.nonzero(
+            assign_result.gt_inds > 0, as_tuple=False).squeeze(-1).unique()
+        neg_inds = torch.nonzero(
+            assign_result.gt_inds == 0, as_tuple=False).squeeze(-1).unique()
+        pos_assigned_gt_inds = assign_result.gt_inds[pos_inds] - 1
+        pos_gt_bboxes = gt_bboxes[pos_assigned_gt_inds.long(), :]
+
+        labels = gt_bboxes.new_full((num_bboxes, ),
+                                    self.num_classes,
+                                    dtype=torch.long)
+
+        labels[pos_inds] = gt_labels[pos_assigned_gt_inds]
+        label_weights = gt_bboxes.new_ones(num_bboxes)
+
+        bbox_targets = torch.zeros_like(bbox_pred, dtype=gt_bboxes.dtype)
+        bbox_weights = torch.zeros_like(bbox_pred, dtype=gt_bboxes.dtype)
+        bbox_weights[pos_inds] = 1.0
+
+        pos_gt_bboxes_normalized = pos_gt_bboxes / factor
+        pos_gt_bboxes_targets = bbox_xyxy_to_cxcywh(pos_gt_bboxes_normalized)
+        bbox_targets[pos_inds] = pos_gt_bboxes_targets
+
+        return (labels, label_weights, bbox_targets, bbox_weights, pos_inds, neg_inds)
+
+    def loss_and_predict(self,
+        hidden_states: Tuple[Tensor],
+        batch_data_samples: SampleList) -> Tuple[dict, InstanceList]:
+
+        batch_gt_instances = []
+        batch_img_metas = []
+        for data_sample in batch_data_samples:
+            batch_img_metas.append(data_sample.metainfo)
+            batch_gt_instances.append(data_sample.gt_instances)
+        
+        outs = self(hidden_states)
+        loss_inputs = outs + (batch_gt_instances, batch_img_metas)
+        losses = self.loss_by_feat(*loss_inputs)
+        
+        predictions = self.predict_by_feat(
+            *outs, batch_img_metas=batch_img_metas)
+        
+        return losses, predictions
+
     def predict(self,
                 hidden_states: Tensor,
                 batch_data_samples: SampleList,
--- projects/detection/tools/test.py
+++ projects/detection/tools/test.py
@@ -3,23 +3,15 @@ import os.path as osp
 
 import torch
 import numpy as np
-from torch.utils.data import DataLoader
-from torchsummary import summary
 from PIL import Image
 
 from mmengine.config import Config
-from mmengine.runner import Runner
-from mmengine.runner import load_checkpoint
-from mmengine.dataset.sampler import DefaultSampler
-from mmengine.dataset import default_collate
-
+from mmengine.runner import Runner, load_checkpoint
 from mmdet.engine.hooks.utils import trigger_visualization_hook
-
-from mmhdmap.registry import DATASETS, MODELS
-from mmengine.visualization import Visualizer
 from mmdet.visualization import DetLocalVisualizer
 
 def parse_args():
+    """Parse command line arguments."""
     parser = argparse.ArgumentParser(
         description='MMDetection test (and eval) a model')
     parser.add_argument('config', help='test config file path')
@@ -28,55 +20,110 @@ def parse_args():
     parser.add_argument('--show', action='store_true', help='show results')
     parser.add_argument('--show-dir', help='directory where painted images will be saved')
     parser.add_argument('--wait-time', type=float, default=2, help='wait time for key press')
-    args = parser.parse_args()
-    return args
+    parser.add_argument('--num-samples', type=int, default=1, 
+                       help='number of samples to visualize (default: 1)')
+    parser.add_argument('--mode', type=str, choices=['test', 'val', 'train'], 
+                       default='test', help='running mode (default: test)')
+    return parser.parse_args()
 
-args = parse_args()
 
-cfg = Config.fromfile(args.config)
+def setup_config(args) -> Config:
+    """Setup and return configuration object."""
+    cfg = Config.fromfile(args.config)
+    
+    # Set work directory
+    if args.work_dir is not None:
+        cfg.work_dir = args.work_dir
+    elif cfg.get('work_dir', None) is None:
+        cfg.work_dir = osp.join(
+            './work_dirs',
+            osp.splitext(osp.basename(args.config))[0])
+    
+    # Set checkpoint path
+    cfg.load_from = args.checkpoint
+    
+    # Setup visualization if needed
+    if args.show:
+        cfg = trigger_visualization_hook(cfg, args)
+    
+    return cfg
 
-if args.work_dir is not None:
-    cfg.work_dir = args.work_dir
-elif cfg.get('work_dir', None) is None:
-    cfg.work_dir = osp.join('./work_dirs',
-                            osp.splitext(osp.basename(args.config))[0])
 
-cfg.load_from = args.checkpoint
-
-if args.show:
-    cfg = trigger_visualization_hook(cfg, args)
-
-runner = Runner.from_cfg(cfg)
-
-def main():
-    visualizer = DetLocalVisualizer()
-    model = runner.model
-    dataloader = runner.test_dataloader
-    # Ensure checkpoint is loaded when running manual loop instead of runner.test()
-    if getattr(cfg, 'load_from', None):
-        load_checkpoint(model, cfg.load_from, map_location='cpu')
-    # set dataset meta for proper class names/palette if available
+def visualize_results(model, dataloader, visualizer, num_samples: int = 1):
+    """Visualize detection results on test samples.
+    
+    Args:
+        model: The detection model.
+        dataloader: The test dataloader.
+        visualizer: The visualizer object.
+        num_samples: Number of samples to visualize.
+    """
+    # Set dataset meta for proper class names/palette if available
     if hasattr(dataloader, 'dataset') and hasattr(dataloader.dataset, 'metainfo'):
         visualizer.dataset_meta = dataloader.dataset.metainfo
+    
+    model.eval()
+    count = 0
+    
     with torch.no_grad():
-        model.eval()
         for data_batch in dataloader:
             output = model.test_step(data_batch)
-            if (output[0].pred_instances is None):
+            
+            # Skip if no predictions
+            if not output or (output[0].pred_instances is None):
                 continue
-            image = np.array(Image.open(output[0].img_path).convert('RGB'))
-            visualizer.add_datasample(
-                osp.basename(output[0].img_path),
-                image,
-                output[0],
-                show=True)
-            break
+            
+            # Process each sample in the batch
+            for sample in output:
+                if sample.pred_instances is None:
+                    continue
+                
+                # Load and visualize image
+                if hasattr(sample, 'img_path') and sample.img_path:
+                    try:
+                        image = np.array(Image.open(sample.img_path).convert('RGB'))
+                        visualizer.add_datasample(
+                            osp.basename(sample.img_path),
+                            image,
+                            sample,
+                            show=True)
+                        count += 1
+                        
+                        if count >= num_samples:
+                            return
+                    except Exception as e:
+                        print(f"Error processing {sample.img_path}: {e}")
+                        continue
 
-def run_val():
-    runner.val()
+
+def main():
+    """Main function for testing."""
+    args = parse_args()
+    cfg = setup_config(args)
+    
+    # Create runner
+    runner = Runner.from_cfg(cfg)
+    
+    # Load checkpoint if specified
+    if getattr(cfg, 'load_from', None):
+        load_checkpoint(runner.model, cfg.load_from, map_location='cpu')
+    
+    # Run based on mode
+    if args.mode == 'test':
+        if args.show:
+            visualizer = DetLocalVisualizer()
+            visualize_results(
+                runner.model, 
+                runner.test_dataloader, 
+                visualizer, 
+                args.num_samples)
+        else:
+            runner.test()
+    elif args.mode == 'val':
+        runner.val()
+    elif args.mode == 'train':
+        runner.train()
 
 
 if __name__ == '__main__':
     main()
-    # run_val()
-  
